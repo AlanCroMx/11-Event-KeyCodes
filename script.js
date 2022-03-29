712ce18fc6872cb0e99a542ca9a9d22b0c63b8b9
Alan Avramovic <alanri1985@gmail.com>
2022-03-29 09:07:33 -0600

    Day11

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+<div class="key">
+${event.key === '' ? 'Space' : event.key}
+ <small>event.KeyCode</small></div>
+
+ 
+<div class="key">
+${event.keyCode} 
+<small>event.keyCode</small></div>
+
+<div class="key">
+${event.code} 
+<small>event.code</small></div>`
+})
